ccedd644200905af91c372695891be00f5bdeb13
Fabio Szostak <fabio.szostak@mirumagency.com>
2021-04-07 19:58:45 -0300

    v0.1.13

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('requirements.txt') as f:
 
 setup(
     name="pwbus-http",
-    version="0.1.12",
+    version="0.1.13",
     author="Fabio Szostak",
     author_email="fszostak@gmail.com",
     description="HTTP Server for PWBUS",
